e1bdf3fe0463334bbea4e21f63b76ac7e1edd585
Peter B. Youssef <Peto.barkar@hotmail.com>
2023-11-28 21:23:04 +0200

    Upload the project files.

---
+++ lang/de.php
@@ -0,0 +1,7 @@
+<?php
+$translations = [
+  "title" => "Aufwertung von Räumen durch fachkundige Gebäudereinigung, Hausmeisterdienste, Garten- und Landschaftsbau-Lösungen",
+  "description" =>  "",
+  "keywords" => "",
+
+];
---
+++ lang/en.php
@@ -0,0 +1,6 @@
+<?php
+$translations = [
+  "title" => "Elevating Spaces with Expert Building Cleaning, Janitorial Services, Gardening, and Landscaping Solutions",
+  "description" =>  "",
+  "keywords" => "",
+];
